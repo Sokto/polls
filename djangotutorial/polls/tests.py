f121b445920136a6014a734669558a43a6dc48cb
Sokto <sisjdbjdcj@gmail.com>
2026-01-09 01:23:57 +0900

    Update tests.py

--- djangotutorial/polls/tests.py
+++ djangotutorial/polls/tests.py
@@ -4,56 +4,34 @@ from django.urls import reverse
 from django.utils import timezone
 from .models import Question
 
-# ----- Тесты для модели Question -----
 class QuestionModelTests(TestCase):
     def test_was_published_recently_with_future_question(self):
-        """
-        was_published_recently() возвращает False для вопросов с pub_date в будущем.
-        """
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date=time)
         self.assertIs(future_question.was_published_recently(), False)
 
     def test_was_published_recently_with_old_question(self):
-        """
-        was_published_recently() возвращает False для вопросов старше 1 дня.
-        """
         time = timezone.now() - datetime.timedelta(days=1, seconds=1)
         old_question = Question(pub_date=time)
         self.assertIs(old_question.was_published_recently(), False)
 
     def test_was_published_recently_with_recent_question(self):
-        """
-        was_published_recently() возвращает True для вопросов, опубликованных за последний день.
-        """
         time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
         recent_question = Question(pub_date=time)
         self.assertIs(recent_question.was_published_recently(), True)
 
-# ----- Тесты для представлений (Views) -----
 def create_question(question_text, days):
-    """
-    Утилита для создания вопроса.
-    `question_text` - текст вопроса.
-    `days` - смещение в днях от now() (отрицательное для прошлого, положительное для будущего).
-    """
     time = timezone.now() + datetime.timedelta(days=days)
     return Question.objects.create(question_text=question_text, pub_date=time)
 
 class QuestionIndexViewTests(TestCase):
     def test_no_questions(self):
-        """
-        Если вопросов нет, отображается соответствующее сообщение.
-        """
         response = self.client.get(reverse('polls:index'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "No polls are available.")
         self.assertQuerySetEqual(response.context['latest_question_list'], [])
 
     def test_past_question(self):
-        """
-        Вопросы с pub_date в прошлом отображаются на index странице.
-        """
         question = create_question(question_text="Past question.", days=-30)
         response = self.client.get(reverse('polls:index'))
         self.assertQuerySetEqual(
@@ -62,18 +40,12 @@ class QuestionIndexViewTests(TestCase):
         )
 
     def test_future_question(self):
-        """
-        Вопросы с pub_date в будущем НЕ отображаются на index странице.
-        """
         create_question(question_text="Future question.", days=30)
         response = self.client.get(reverse('polls:index'))
         self.assertContains(response, "No polls are available.")
         self.assertQuerySetEqual(response.context['latest_question_list'], [])
 
     def test_future_question_and_past_question(self):
-        """
-        Даже если существуют и прошлые и будущие вопросы, показываются только прошлые.
-        """
         question = create_question(question_text="Past question.", days=-30)
         create_question(question_text="Future question.", days=30)
         response = self.client.get(reverse('polls:index'))
@@ -83,32 +55,24 @@ class QuestionIndexViewTests(TestCase):
         )
 
     def test_two_past_questions(self):
-        """
-        На index странице может отображаться несколько вопросов.
-        """
         question1 = create_question(question_text="Past question 1.", days=-30)
         question2 = create_question(question_text="Past question 2.", days=-5)
         response = self.client.get(reverse('polls:index'))
         self.assertQuerySetEqual(
             response.context['latest_question_list'],
-            [question2, question1],  # Порядок должен быть от новых к старым
+            [question2, question1],
         )
 
 class QuestionDetailViewTests(TestCase):
     def test_future_question(self):
-        """
-        DetailView вопроса с pub_date в будущем возвращает 404.
-        """
         future_question = create_question(question_text='Future question.', days=5)
         url = reverse('polls:detail', args=(future_question.id,))
         response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
 
     def test_past_question(self):
-        """
-        DetailView вопроса с pub_date в прошлом отображает текст вопроса.
-        """
         past_question = create_question(question_text='Past Question.', days=-5)
         url = reverse('polls:detail', args=(past_question.id,))
         response = self.client.get(url)
+
         self.assertContains(response, past_question.question_text)
